39c828285a7d9e1788125af4065af65f72e3d638
Lucas <lucas.vergara@live.com>
2024-04-09 23:20:52 -0300

    :construction: Create Fetch from api

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('products starter');
+
+const url = 'https://course-api.com/javascript-store-products';
+
+const produtcsDOM = document.querySelector('.products-center')
+
+const fetchProducts = async () => {
+    produtcsDOM.innerHTML = 
+    try {
+        const resp = await fetch (url);
+        const data = await resp.json()
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchProducts();
