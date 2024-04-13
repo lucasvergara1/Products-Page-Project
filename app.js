e3657f8e22055148bae92f76a0e37e8412429cb0
Lucas <lucas.vergara@live.com>
2024-04-12 22:10:36 -0300

    :construction: Create Loading image and Error message

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const url = 'https://course-api.com/javascript-store-products';
 const produtcsDOM = document.querySelector('.products-center')
 
 const fetchProducts = async () => {
-    produtcsDOM.innerHTML = 
+    produtcsDOM.innerHTML = '<div class ="loading"></div>';
     try {
         const resp = await fetch (url);
         const data = await resp.json()
         console.log(data);
     } catch (error) {
-        console.log(error);
+        produtcsDOM.innerHTML = '<p class="error">there was an error</p>';
     }
-}
+};
 
 fetchProducts();
