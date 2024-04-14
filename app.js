808d7d049306c1a7c3811b469648e2e550e8433b
Lucas <lucas.vergara@live.com>
2024-04-13 22:26:41 -0300

    :construction: Create display products

--- app.js
+++ app.js
@@ -7,11 +7,39 @@ const fetchProducts = async () => {
     produtcsDOM.innerHTML = '<div class ="loading"></div>';
     try {
         const resp = await fetch (url);
-        const data = await resp.json()
-        console.log(data);
+        const data = await resp.json();
+        return data
     } catch (error) {
         produtcsDOM.innerHTML = '<p class="error">there was an error</p>';
     }
 };
 
-fetchProducts();
+
+const displayProducts = (list) =>{
+    const productList = list.map((product) => {
+       const{id} = product;
+       const {name: title, price} = product.fields;
+       const {url: img} = product.fields.image[0];
+       const formatPrice = price / 100;
+        //id, name, price, img
+        return `<a class="single-product" href="product.html">
+            <img src="${img}" class="single-product-img 
+            img" alt="${title}"/>
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">${formatPrice}</span>
+            </footer>
+          </a>`;
+    }).join('');
+    produtcsDOM.innerHTML = `<div class="products-container">
+        ${productList}
+        </div>`;
+};
+
+const start = async() =>{
+    const data = await fetchProducts()
+    displayProducts(data);
+};
+
+start();
+
