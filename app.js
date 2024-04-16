37c125659f58b9dbb8757365912b21898b08abe2
Lucas <lucas.vergara@live.com>
2024-04-15 23:06:34 -0300

    :construction: Create HTML single product

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
+const url = "https://course-api.com/javascript-store-products";
 
-const url = 'https://course-api.com/javascript-store-products';
-
-const produtcsDOM = document.querySelector('.products-center')
+const produtcsDOM = document.querySelector(".products-center");
 
 const fetchProducts = async () => {
-    produtcsDOM.innerHTML = '<div class ="loading"></div>';
-    try {
-        const resp = await fetch (url);
-        const data = await resp.json();
-        return data
-    } catch (error) {
-        produtcsDOM.innerHTML = '<p class="error">there was an error</p>';
-    }
+  produtcsDOM.innerHTML = '<div class ="loading"></div>';
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    produtcsDOM.innerHTML = '<p class="error">there was an error</p>';
+  }
 };
 
-
-const displayProducts = (list) =>{
-    const productList = list.map((product) => {
-       const{id} = product;
-       const {name: title, price} = product.fields;
-       const {url: img} = product.fields.image[0];
-       const formatPrice = price / 100;
-        //id, name, price, img
-        return `<a class="single-product" href="product.html">
+const displayProducts = (list) => {
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      //id, name, price, img
+      return `<a class="single-product" href="product.html?id=${id}&name=john&age=25">
             <img src="${img}" class="single-product-img 
             img" alt="${title}"/>
             <footer>
@@ -30,16 +29,16 @@ const displayProducts = (list) =>{
               <span class="price">${formatPrice}</span>
             </footer>
           </a>`;
-    }).join('');
-    produtcsDOM.innerHTML = `<div class="products-container">
+    })
+    .join('');
+  produtcsDOM.innerHTML = `<div class="products-container">
         ${productList}
         </div>`;
 };
 
-const start = async() =>{
-    const data = await fetchProducts()
-    displayProducts(data);
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
 };
 
 start();
-
