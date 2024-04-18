353b547e53894552998c3a45fce2dee334b65ed5
Lucas <lucas.vergara@live.com>
2024-04-17 21:58:38 -0300

    :construction: working in progress

--- product.js
+++ product.js
@@ -1,10 +1,21 @@
 const productDOM = document.querySelector('.product');
 console.log(productDOM);
 
-const url = 'https://www.course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
+const url = 'https://www.course-api.com/javascript-store-single-product';
 
 const fetchProduct = async() =>{
-    return 'product'
+   try {
+    productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>'
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+   
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json()
+    return data
+   } catch (error) {
+    productDOM.innerHTML = '<p class="error">There was a problem loading the product. Please try again later</p>';
+
+   }
 }
 
 const displayProducts = (product) =>{
