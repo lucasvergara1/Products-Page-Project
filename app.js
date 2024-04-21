3e8f944199c11ec4356b2977950689e493774be2
Lucas <lucas.vergara@live.com>
2024-04-20 22:04:06 -0300

    :construction: Working in progress

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const url = "https://course-api.com/javascript-store-products";
 
-const produtcsDOM = document.querySelector(".products-center");
+const productsDOM = document.querySelector(".products-center");
 
 const fetchProducts = async () => {
-  produtcsDOM.innerHTML = '<div class ="loading"></div>';
+  productsDOM.innerHTML = '<div class="loading"></div>';
   try {
     const resp = await fetch(url);
     const data = await resp.json();
     return data;
   } catch (error) {
-    produtcsDOM.innerHTML = '<p class="error">there was an error</p>';
+    productsDOM.innerHTML = '<p class="error">there was an error</p>';
   }
 };
 
@@ -20,19 +20,19 @@ const displayProducts = (list) => {
       const { name: title, price } = product.fields;
       const { url: img } = product.fields.image[0];
       const formatPrice = price / 100;
-      //id, name, price, img
+      // id,name,price,img
       return `<a class="single-product" href="product.html?id=${id}&name=john&age=25">
-            <img src="${img}" class="single-product-img 
-            img" alt="${title}"/>
+            <img src="${img}" class="single-product-img img" alt="${title}" />
             <footer>
               <h5 class="name">${title}</h5>
-              <span class="price">${formatPrice}</span>
+              <span class="price">$${formatPrice}</span>
             </footer>
           </a>`;
     })
-    .join('');
-  produtcsDOM.innerHTML = `<div class="products-container">
-        ${productList}
+    .join("");
+  productsDOM.innerHTML = ` <div class="products-container">
+         ${productList}
+          
         </div>`;
 };
 
